aa8aa12b3dd28f57453e56632e3cbcdae5f932f8
Charl Janse van Rensburg <charljvr121@gmail.com>
2020-07-01 16:49:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: Map<TargetPlatform,
+                _InanimatePageTransitionsBuilder>.fromIterable(
+              TargetPlatform.values.toList(),
+              key: (dynamic k) => k,
+              value: (dynamic _) => const _InanimatePageTransitionsBuilder(),
+            ),
+          ),
+        ),
+        initialRoute: RouteNames.home,
+        navigatorObservers: [
+          AppRouteObserver()
+        ],
+        routes: {
+          RouteNames.home: (_) => const HomePage(),
+        });
+  }
+}
+
+class _InanimatePageTransitionsBuilder extends PageTransitionsBuilder {
+  const _InanimatePageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
+  }
+}
